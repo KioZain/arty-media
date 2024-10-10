3dc68ea1edd7219e3b93ba27d06494c8b0f8e80f
maxim_kio <enderbroxa@yandex.ru>
2024-10-10 20:02:13 +0300

    Git push

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
-    @collection = Collection.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   # GET /posts/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
-  resources :collections do
-    resources :posts
-  end
+  get "static_pages/home"
+  get "static_pages/about"
+  get "/about", to: "static_pages#about" # Маршрут для страницы about
 
+
+  resources :collections
   resources :posts do
     resources :comments
   end
 
+  # root path
+  root "static_pages#home"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  root "posts#index"
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+# Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+# Can be used by load balancers and uptime monitors to verify that the app is live.
+# Defines the root path route ("/")
