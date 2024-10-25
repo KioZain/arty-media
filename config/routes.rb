ca6caa755ca22aee84e524cb3743304ef83474da
maxim_kio <enderbroxa@yandex.ru>
2024-10-25 12:48:36 +0300

    change

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   # root path
-
+  root "static_pages#home"
 
 
   get "up" => "rails/health#show", as: :rails_health_check
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
 
-  root "static_pages#home"
+  
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
